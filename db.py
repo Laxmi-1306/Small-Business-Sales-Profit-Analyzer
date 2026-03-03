4e3bba7641efa7a684b3cb52cb31ec7cefd6cfc8
LAXMI S M <laxmisubramaniam13@gmail.com>
2026-03-03 21:21:01 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,23 @@
 import sqlite3
 
 def get_db():
-    conn = sqlite3.connect("sales.db", check_same_thread=False)
+    conn = sqlite3.connect("database.db", check_same_thread=False)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password TEXT
+        )
+    """)
+    
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS sales (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            product TEXT,
+            revenue REAL,
+            cost REAL
+        )
+    """)
+    
     return conn
