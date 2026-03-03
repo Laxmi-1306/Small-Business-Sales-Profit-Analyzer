6ee63b1cddffdf53375a94f016b6651011d4ae31
LAXMI S M <laxmisubramaniam13@gmail.com>
2026-03-03 21:21:26 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,13 +6,13 @@ SECRET_KEY = "secret123"
 def create_token(user_id):
     payload = {
         "user_id": user_id,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=5)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
 def verify_token(token):
     try:
-        data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        return data["user_id"]
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload["user_id"]
     except:
         return None
